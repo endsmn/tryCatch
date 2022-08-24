2ae170e8a05b64bb10941d627c7b3b8e5a2b861a
Omer Ozekin <ozekinomer@gmail.com>
2022-08-24 08:33:12 +0300

    homework

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace tryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Bir Sayı Giriniz:");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girmiş Olduğunuz Sayı:" + sayi);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata:" + ex.Message.ToString());
+            }
+            // finally
+            // {
+            //      Console.WriteLine("İşlem Tamamlandı.");
+            // }
+
+            try
+            {
+                //int a = int.Parse(null);
+                //int a = int.Parse("test");
+                int a = int.Parse("-2000000000000");
+
+
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş Değer Girdiniz.");
+                Console.WriteLine(ex);
+
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Veri tipi uygun değil.");
+                Console.WriteLine(ex);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Çok küçük yada büyük bir değer girdiniz.");
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine("İşlem Tamamlandı.");
+            }
+
+
+
+        }
+    }
+}
